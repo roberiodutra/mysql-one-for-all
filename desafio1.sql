25789ff165a4a3a7334a2d0a8b255d301c90284e
roberio <roberio-almeida@hotmail.com>
2022-06-05 21:43:07 -0300

    feat: insert users infos into users table

--- desafio1.sql
+++ desafio1.sql
@@ -65,3 +65,16 @@ VALUES
   ('family', 7.99),
   ('university', 5.99),
   ('personal', 6.99);
+
+INSERT INTO users (name, age, plan_id, signature_date)
+VALUES 
+  ('Thati', 23, 1, '2019-10-20'),
+  ('Cintia', 35, 2, '2017-12-30'),
+  ('Bill', 20, 3, '2019-06-05'),
+  ('Roger', 45, 4, '2020-05-13'),
+  ('Norman', 58, 4, '2017-02-17'),
+  ('Patrick', 33, 2, '2017-01-06'),
+  ('Vivian', 26, 3, '2018-01-05'),
+  ('Carol', 19, 3, '2018-02-14'),
+  ('Angelina', 42, 2, '2018-04-29'),
+  ('Paul', 46, 2, '2017-01-17');
