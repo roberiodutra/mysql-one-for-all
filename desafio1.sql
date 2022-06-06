432ab994544f3c0c77d28d1d4eb02cd8128bd493
roberio <roberio-almeida@hotmail.com>
2022-06-05 21:39:03 -0300

    feat: create users table

--- desafio1.sql
+++ desafio1.sql
@@ -9,3 +9,12 @@ CREATE TABLE plans(
     name VARCHAR(64) NOT NULL,
     value DECIMAL(4, 2) NOT NULL
 ) engine = InnoDB;
+
+CREATE TABLE users(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(64) NOT NULL,
+  age INT NOT NULL,
+  plan_id INT NOT NULL,
+  signature_date DATE NOT NULL,
+  FOREIGN KEY (plan_id) REFERENCES plans(id)
+) engine = InnoDB;
